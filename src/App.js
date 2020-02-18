7a0c9fbd629df3845f3ed9fa14ec552b8ac4cad6
Adiomojeed <adio.mojeed@gmail.com>
2020-02-18 22:42:52 +0100

    configured ESLint, worked with hooks and installed the pet API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Pet from './Pet';
+import SearchParams from './SearchParams'
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  )
 };
 
-ReactDOM.render(React.createElement(App), document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
