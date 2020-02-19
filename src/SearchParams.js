694592d758eeade486b2c9ea6cfa8a81f477198e
Adiomojeed <adio.mojeed@gmail.com>
2020-02-19 22:17:52 +0100

    Added browser compatibility

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -6,11 +6,28 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([])
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS)
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds)
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds)
+  const [pets, setPets] = useState([])
   
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+
+    setPets(animals || [])
+  }
+
   useEffect( () => {
-    pet.breeds('dog').then(console.log, console.error)
-  })
+    setBreeds([])
+    setBreed('')
+    
+    pet.breeds(animal).then(({breeds}) => {
+      const breedStrings = breeds.map((breedObj) => breedObj.name)
+      setBreeds(breedStrings)
+    }, console.error)
+  }, [animal, setBreed, setBreeds])
   return (
     <div className="search-params">
       <form>
